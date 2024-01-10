ac7dc021c7be270fb211f97e8a1e3584135d0487
vincentRouget <vincent.rouget86@gmail.com>
2024-01-10 16:44:04 +0100

    First commit

---
+++ src/main/java/com/wildcodeschool/myProject/controller/DoctorController.java
@@ -0,0 +1,29 @@
+package com.wildcodeschool.myProject.controller;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DoctorController {
+    @GetMapping("/doctor/1")
+
+    @ResponseBody
+    public String doctor1() {
+        return "William Hartnell";
+    }
+
+    @GetMapping("/doctor/10")
+    @ResponseBody
+    public String doctor10() {
+        return "David Tennant";
+    }
+
+    @GetMapping("/doctor/13")
+    @ResponseBody
+    public String doctor13() {
+        return "Jodie Whittaker";
+    }
+}
